6ac5576c0a06da0630f6809ad869f032ebf114e2
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-09-20 13:19:47 -0400

    feat: project entity created

---
+++ src/main/java/br/com/pedroart/projectsmanager/model/project.java
@@ -0,0 +1,12 @@
+package br.com.pedroart.projectsmanager.model;
+
+import java.time.LocalDate;
+public class project {
+    private int idProject;
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String status;
+    private double budget;
+}
