30ec1f3a72abc143e0ea7e8cc416de1ad15603c5
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-09-20 13:08:49 -0400

    feat: database connector add

--- src/main/java/br/com/pedroart/projectsmanager/utils/DatabaseConnector.java
+++ src/main/java/br/com/pedroart/projectsmanager/utils/DatabaseConnector.java
@@ -1,5 +1,15 @@
 package br.com.pedroart.projectsmanager.utils;
 
-public class DatabaseConnector {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DatabaseConnector {
+    private static final String URL = "jdbc:mysql://localhost:3306/seu_banco_de_dados?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "seu_usuario_mysql";
+    private static final String PASSWORD = "sua_senha_mysql";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
