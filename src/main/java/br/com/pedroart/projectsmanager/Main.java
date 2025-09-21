5494a0900158f0586a2bdcbbf112dfd28d711319
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-09-21 15:18:59 -0400

    feat: default program launcher

--- src/main/java/br/com/pedroart/projectsmanager/Main.java
+++ src/main/java/br/com/pedroart/projectsmanager/Main.java
@@ -1,29 +1,20 @@
 package br.com.pedroart.projectsmanager;
 
-import br.com.pedroart.projectsmanager.dao.ProjectDAO;
-import br.com.pedroart.projectsmanager.exception.DataAccessException;
-import br.com.pedroart.projectsmanager.model.Project;
-import java.time.LocalDate;
+import br.com.pedroart.projectsmanager.view.ProjectManagerGUI;
+import javax.swing.SwingUtilities;
 
 public class Main {
+
     public static void main(String[] args) {
-        
-        ProjectDAO projetoDAO = new ProjectDAO();
-        Project newProject = new Project();
-        newProject.setName("Desenvolvimento do App Mobile");
-        newProject.setDescription("Criar o novo aplicativo de vendas da empresa.");
-        newProject.setStartDate(LocalDate.now());
-        newProject.setEndDate(LocalDate.of(2026, 3, 31));
-        newProject.setBudget(50000.00);
-
-        try {
-            projetoDAO.create(newProject);
-            System.out.println("Operation completed successfully!");
-
-        } catch (DataAccessException e) {
-            System.err.println("A database error occurred. Operation canceled.");
-        } catch (Exception e) {
-            System.err.println("An unexpected error occurred in the application: " + e.getMessage());
-        }
+        SwingUtilities.invokeLater(() -> {
+            try {
+                System.setProperty("awt.useSystemAAFontSettings", "on");
+                System.setProperty("swing.aatext", "true");
+                
+                new ProjectManagerGUI().setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
